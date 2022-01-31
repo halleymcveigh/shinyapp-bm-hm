d17739eb453d87c10d8cb4c3afad0afda18dc3fe
Halley McVeigh <hmcveigh@ucsb.edu>
2022-01-31 11:22:59 -0800

    initial commit, barebones structure (no data yet)

---
+++ Shiny App - Tahoe.R
@@ -0,0 +1,45 @@
+# attach packages
+library(shiny)
+library(tidyverse)
+
+
+# create the user interface
+ui <- fluidPage(theme = "tahoe.css",
+                navbarPage(
+                  "Our app title - Blue Forest Conservation",
+                  tabPanel("Ecosystem benefits",
+                           sidebarLayout(
+                             sidebarPanel(
+                               "WIDGET 1",
+                               checkboxGroupInput(inputId = "ecosystem_service",
+                                                  label = "Select ecosystem service:",
+                                                  choices = unique(data$)
+                                                  ) # end checkboxGroupInput
+                             ), #end of sidebarPanel
+                             mainPanel(
+                               "OUTPUT MAP GOES HERE",
+                               plotOutput("eco_ben_map")
+                             ) #end of mainPanel
+                           ) #end of sidebarLayout
+                           ),
+                  tabPanel("Risks to ecosystem services"),
+                  tabPanel("Priority areas")
+                ) #end of navbarPage
+                ) #end ui
+
+
+# create server function
+server <- function(input, output) {
+  eco_ben_reactive <- reactive({
+    data %>% 
+      filter()
+  }) #end sw_reactive
+  
+  output$eco_ben_reactive <- renderPlot(
+    
+  )
+}
+
+# combine into an app:
+
+shinyApp(ui = ui, server = server)
