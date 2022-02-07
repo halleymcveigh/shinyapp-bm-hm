f114e5b1eaf76f61a0b116ec5f0a739d0d089058
Halley McVeigh <hmcveigh@ucsb.edu>
2022-02-07 11:52:50 -0800

    tried to add ecosystem benefits csv, not sure what i'm missing

--- Shiny App - Tahoe.R
+++ Shiny App - Tahoe.R
@@ -1,19 +1,21 @@
 # attach packages
 library(shiny)
 library(tidyverse)
+library(here)
 
+benefits <- read.csv(here("data", "benefits_categories.csv"))
 
 # create the user interface
 ui <- fluidPage(theme = "tahoe.css",
                 navbarPage(
-                  "Our app title - Blue Forest Conservation",
+                  "Our app title - Blue Forest Conservation in Tahoe",
                   tabPanel("Ecosystem benefits",
                            sidebarLayout(
                              sidebarPanel(
                                "WIDGET 1",
                                checkboxGroupInput(inputId = "ecosystem_service",
                                                   label = "Select ecosystem service:",
-                                                  choices = unique(data$)
+                                                  choices = unique(benefits$What.forest.benefit.does.your.organization.value.in.this.area.)
                                                   ) # end checkboxGroupInput
                              ), #end of sidebarPanel
                              mainPanel(
