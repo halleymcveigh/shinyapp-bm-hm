83c90c3128e60d17dbae1b86f857d69cedff93ce
Halley McVeigh <hmcveigh@ucsb.edu>
2022-02-07 17:17:15 -0800

    added text

--- Shiny App - Tahoe.R
+++ Shiny App - Tahoe.R
@@ -8,7 +8,7 @@ benefits <- read.csv(here("data", "benefits_categories.csv"))
 # create the user interface
 ui <- fluidPage(theme = "tahoe.css",
                 navbarPage(
-                  "Our app title - Blue Forest Conservation in Tahoe",
+                  "Visualizing Ecosystem Services of Interest in the Tahoe-Central Sierra Region - Blue Forest Conservation",
                   tabPanel("Ecosystem benefits",
                            sidebarLayout(
                              sidebarPanel(
@@ -19,13 +19,17 @@ ui <- fluidPage(theme = "tahoe.css",
                                                   ) # end checkboxGroupInput
                              ), #end of sidebarPanel
                              mainPanel(
-                               "OUTPUT MAP GOES HERE",
+                               "The Tahoe-Central Sierra region is a diverse landscape that covers approximately 2.4 million acres of land, multiple National Forests, Wildernesses, cities and cities. The region is rich in diversity, but under threat from climate change impacts. This study sought to determine specific stakeholder interest in various ecosystem services to better prioritize how to protect these services under changing climate and future management scenarios...
+                               OUTPUT MAP GOES HERE
+                               with a description of how participants were asked to identify regions of interest within our study region
+                               NEED TO FIGURE OUT HOW TO INSERT MAP AND PLOT SHAPEFILES OF STUDY REGION HERE",
                                plotOutput("eco_ben_reactive_plot")
                              ) #end of mainPanel
                            ) #end of sidebarLayout
                            ),
-                  tabPanel("Risks to ecosystem services"),
-                  tabPanel("Priority areas")
+                  tabPanel("Impacts/Risks to Ecosystem Services"),
+                  tabPanel("Threats to Ecosystem Services"),
+                  tabPanel("Priority Management Areas")
                 ) #end of navbarPage
                 ) #end ui
 
